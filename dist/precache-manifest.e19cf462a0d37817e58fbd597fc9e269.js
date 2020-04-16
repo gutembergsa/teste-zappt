eeb054a6560af3e860ac888844cc0fe41983c29f
Gutemberg Sá <gutemberg.silvasa@outlook.com>
2020-04-16 15:20:09 -0300

    deploy

---
+++ dist/precache-manifest.e19cf462a0d37817e58fbd597fc9e269.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61bcedefef64910edddf",
+    "url": "/css/app.cf5fe2af.css"
+  },
+  {
+    "revision": "cfacd0ce7e5b04eae6c7",
+    "url": "/css/chunk-vendors.eb2beebe.css"
+  },
+  {
+    "revision": "a8cb4a3f1a2ea97b5619baf7432c8fb3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "61bcedefef64910edddf",
+    "url": "/js/app.31fce9b5.js"
+  },
+  {
+    "revision": "cfacd0ce7e5b04eae6c7",
+    "url": "/js/chunk-vendors.cda3e356.js"
+  },
+  {
+    "revision": "f6ffd2297e835a753e8c909b44e4eebc",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
